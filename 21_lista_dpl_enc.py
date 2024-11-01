7133e2e49182181fe14694e8181d7d07a7eb0c3d
Claudio-Fatec <162808729+Claudio-Fatec@users.noreply.github.com>
2024-11-01 19:44:27 -0300

    acerto gerais

--- 21_lista_dpl_enc.py
+++ 21_lista_dpl_enc.py
@@ -33,6 +33,24 @@ print(lista)
 primeira = lista.peek(0)
 pos5 = lista.peek(5)
 ultima = lista.peek(-1)
-penultima = lista.pee(-2)
+penultima = lista.peek(-2)
 
 print(f"PRIMEIRA FRUTA: {primeira}; FRUTA NA POS. 5:{pos5}; ÚLTIMA: {ultima}; PENÚLTIMA: {penultima}")
+
+print(lista)
+
+# Fazendo remoções
+rem_primeira = lista.remove(0)
+print(f"Fruta removida de primeira posição: {rem_primeira}")
+
+rem_ultima = lista.pop()  # equevalente a lista.remove(lista.count() -1)
+print(f"Frutaremovida da últma posição: {rem_ultima}")
+
+print(lista)
+
+rem_pos3 = lista.remove(3)
+print(f"Fruta removidada posição 3: {rem_pos3}")
+
+print(lista)
+
+print(f"Quantidade finalde itens na lista: {lista.count()}")
